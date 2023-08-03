9aaf313811d9c42458c9ebd276df757521dc9077
Musksticks <62134419+Musksticks@users.noreply.github.com>
2023-08-03 20:38:21 +0930

    updated handling / notes

--- main.py
+++ main.py
@@ -1,94 +1,99 @@
 from random import randint
 from colorama import Fore
 
+# VARIABLES
 turn = 0
-playerturn = 0
-gameinitiated = 0
-defaultamount = 1000
-totalamount = defaultamount
-
+stand = False
+gameInitiated = 0
+DEFAULT_MONEY = 1000
+totalAmount = DEFAULT_MONEY
 
 def hit():
-    number = randint(2, 10)
-    return number
+    return randint(2, 10)
 
 while True:
-
     turn += 1
 
     try:
-        if gameinitiated == 0:
-            if totalamount <= 0:
+        if gameInitiated == 0:
+            # OUT OF MONEY HANDLING
+            if totalAmount <= 0:
                 ans = input(Fore.RED + "You are broke, Restart the game? (Y)es, (N)o: " + Fore.RESET)
                 if ans.upper() == "Y":
-                    totalamount = defaultamount
+                    totalAmount = DEFAULT_MONEY
                 if ans.upper() == "N":
                     break
                 else:
                     continue
 
-            print("Balance:", totalamount)
-            amount = int(input("How much would you like to bet?: "))
-            if amount > totalamount or amount < 0:
+            # START OF BET / GAME
+            print("Balance:", totalAmount)
+            amount = int(input(Fore.GREEN + "How much would you like to bet?: " + Fore.RESET))
+            if amount > totalAmount or amount < 0:
                 print("Invalid amount")
                 continue
-            totalamount -= amount
-            gameinitiated = 1
+            totalAmount -= amount
+            gameInitiated = 1
+            playerturn = 0
             p1total = 0
             p2total = 0
             turn = 0
-            playerturn = 0
 
-        if gameinitiated == 1:
-
+        # BUST HANDLING
+        if gameInitiated == 1:
             if p1total > 21:
-                gameinitiated = 0
+                gameInitiated = 0
                 playerturn = -1
                 print("Bust in", turn, "turn(s)!")  # Removed money handling on loss
             if p2total > 21:
-                gameinitiated = 0
+                gameInitiated = 0
                 playerturn = -1
                 print("Dealer bust!")
                 amount *= 2
-                totalamount += amount
+                totalAmount += amount
 
-            if playerturn == 0:
+            # ASK PLAYER (H)IT OR (S)TAND
+            if playerturn == 0 and not stand:
                 roll = str(input(Fore.BLUE + "Would you like to (H)it or (S)tand: " + Fore.RESET))  # "Please draw a card: (Y)es , (N)o"
                 if roll.upper() == "H":
                     p1total += hit()
                     print("Your total:", p1total)
                 elif roll.upper() == "S":
-                    playerturn += 1
+                    playerturn = 1
+                    stand = True
                     continue
-
-            elif playerturn == 1:
+            
+            # CHECK FOR HIT / STAND & START OF DEALER AI
+            elif playerturn == 1 or stand:
                 if not p1total > 0:
                     playerturn = 0
+                    stand = False
                     print("Please hit atleast once")
                     continue
                 if p2total <= 16:
-                    print("Dealer: Hit")
-                    playerturn -= 1
+                    playerturn = 0
                     p2total += hit()
-                    print("Dealer:", p2total)
+                    print("Dealer hit:", p2total)
 
+            # DEALER / PLAYER WIN HANDLING
             if p2total >= 17 and p2total < 22:
-                print("Dealer: Stand")
-                playerturn -= 1
+                print("Dealer stand:", p2total)
+                playerturn = 0
                 if p1total > p2total and p1total < 22:
                     print("You:", p1total, "Dealer:", p2total)
                     print("You win in", turn, "turns!")
                     amount *= 2
-                    totalamount += amount
-                    gameinitiated = 0
+                    totalAmount += amount
+                    gameInitiated = 0
                 elif p2total > p1total and p2total < 22:
                     print("You:", p1total, "Dealer:", p2total)
                     print("You lose in", turn, "turns!")  # Removed money handling on loss
-                    gameinitiated = 0
+                    gameInitiated = 0
                 elif p1total == p2total:
+                    print("You:", p1total, "Dealer:", p2total)
                     print("You both draw in", turn, "turns!")
-                    gameinitiated = 0
-                    totalamount += amount
+                    gameInitiated = 0
+                    totalAmount += amount
 
     except ValueError:
         print("Invalid Entry")
